62a2ce3b62fb7176a7049eb4d38137ebb18f63a2
fakhriyya1 <bagirovafexriyye5@gmail.com>
2023-03-25 15:47:36 +0400

    header

--- assets/js/header.js
+++ assets/js/header.js
@@ -15,3 +15,34 @@ toggleMenu.addEventListener("click", () => {
 closeMenu.addEventListener("click", () => {
   navMenu.classList.remove("show_menu");
 });
+
+let media = window.matchMedia("(max-width: 1200px)");
+
+if (media.matches) {
+  document.querySelector("nav").style.height = "4rem";
+  document.getElementById("navbar").style.display = "none";
+  document.querySelector(".navbar_responsive").style.display = "flex";
+}
+else {
+  // document.querySelector("nav").style.height = "8rem";
+  // document.getElementById("navbar").style.display = "block";
+  window.onscroll = function () {
+  scrollFunction();
+};
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.display = "none";
+    document.querySelector("nav").style.height = "4rem";
+    document.querySelector(".megamenu").style.top = "4rem";
+    document.querySelector(".icons_scroll").style.display = "block";
+    document.querySelector(".icons_scroll").style.top = "1.5rem";
+  } else {
+    document.getElementById("navbar").style.display = "flex";
+    document.querySelector("nav").style.height = "8rem";
+    document.querySelector(".megamenu").style.top = "8rem";
+    document.querySelector(".icons_scroll").style.display = "none";
+    document.querySelector(".icons_scroll").style.top = "5rem";
+  }
+}
