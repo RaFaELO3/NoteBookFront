2f5136706f447d1b15e1188e9113b5791c4abb11
neegar-09 <115188548+neegar-09@users.noreply.github.com>
2023-04-27 21:30:43 +0400

    filter and detail fav button
    filter and detail fav button

--- card/card.js
+++ card/card.js
@@ -81,4 +81,13 @@ function carousel() {
 
 }
 
+//fav buton
+let favbtn = document.querySelector(".favbtn")
+let fav = document.querySelectorAll(".fav");
+favbtn.addEventListener("click" , ()=>{
+    fav.forEach(item=>{
+        item.classList.toggle("show")
+    })
+})
+
 
